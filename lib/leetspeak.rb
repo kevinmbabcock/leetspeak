17fd733f3b3e463d882761f538ae1437fdcbef87
Kevin Babcock and Shane Robinson <kevin+shane@>
2017-11-13 15:00:31 -0800

    replaces o with 0.

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -2,9 +2,13 @@ def leetspeak(word)
   new_array = []
   word_split = word.split(//)
   word_split.each() do |letter|
-    if (letter == "e")
+    if (letter == "e" || letter == "E")
       letter.replace("3")
     end
+    if (letter == "o" || letter == "O")
+      letter.replace("0")
+    end
+
   end
   word_split.join("")
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -6,4 +6,7 @@ describe('leetspeak') do
   it("all letter e will be changed to 3") do
     expect(leetspeak("Steve")).to(eq("St3v3"))
   end
+  it("all letter o will be changed to 0") do
+    expect(leetspeak("hello")).to(eq("h3ll0"))
+  end
 end
